bbad2c60de5f752feb82d80305c7ee58b8619c1a
Veal98 <1912420914@qq.com>
2023-08-26 18:29:11 +0800

    refactor dependency

--- ddd-ezlink-client/src/main/java/cn/itmtx/ddd/ezlink/client/dto/data/UrlMapDTO.java
+++ ddd-ezlink-client/src/main/java/cn/itmtx/ddd/ezlink/client/dto/data/UrlMapDTO.java
@@ -2,8 +2,15 @@ package cn.itmtx.ddd.ezlink.client.dto.data;
 
 import lombok.Data;
 
-@Data
 public class UrlMapDTO {
 
     String shortUrl;
+
+    public String getShortUrl() {
+        return shortUrl;
+    }
+
+    public void setShortUrl(String shortUrl) {
+        this.shortUrl = shortUrl;
+    }
 }
