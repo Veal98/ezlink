082cacfa7c69a594f34df862f151c279f6e4b00e
Veal98 <1912420914@qq.com>
2023-11-18 12:43:43 +0800

    update dubbo configs for provider

--- ddd-ezlink-adapter/src/main/java/cn/itmtx/ddd/ezlink/adapter/rpc/UrlMapProvider.java
+++ ddd-ezlink-adapter/src/main/java/cn/itmtx/ddd/ezlink/adapter/rpc/UrlMapProvider.java
@@ -6,8 +6,12 @@ import cn.itmtx.ddd.ezlink.client.api.UrlMapService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
+/**
+ * 注册 RPC 接口
+ */
 @Slf4j
 @DubboService(timeout = RPC.DEFAULT_TIMEOUT)
 public class UrlMapProvider extends UrlMapServiceImpl implements UrlMapService {
+
 
 }
--- ddd-ezlink-app/src/main/java/cn/itmtx/ddd/ezlink/application/service/UrlMapServiceImpl.java
+++ ddd-ezlink-app/src/main/java/cn/itmtx/ddd/ezlink/application/service/UrlMapServiceImpl.java
@@ -7,9 +7,16 @@ import cn.itmtx.ddd.ezlink.client.dto.data.UrlMapDTO;
 import cn.itmtx.ddd.ezlink.client.dto.query.LongByShortQry;
 import com.alibaba.cola.dto.SingleResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 
+
+/**
+ * 这里起个 BeanName="urlMapService"，不然自动注入的时候会和 UrlMapProvider 冲突
+ * 或者用 @Primary 注解也可以
+ */
 @Service
+@Primary
 public class UrlMapServiceImpl implements UrlMapService {
 
     @Autowired
