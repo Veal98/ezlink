8075c14a58d45395effefde8f791c15b9b59f117
Veal98 <1912420914@qq.com>
2024-02-13 11:05:34 +0800

    update mono test

--- start/src/test/java/cn/itmtx/ddd/ezlink/MonoTest.java
+++ start/src/test/java/cn/itmtx/ddd/ezlink/MonoTest.java
@@ -1,4 +1,4 @@
-package cn.itmtx.ddd.ezlink.application;
+package cn.itmtx.ddd.ezlink;
 
 import cn.itmtx.ddd.ezlink.start.Application;
 import org.junit.Test;
@@ -7,6 +7,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import reactor.core.publisher.Mono;
 
+/**
+ * 集成测试，需要确保环境都正常启动
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class MonoTest {
--- start/src/test/java/cn/itmtx/ddd/ezlink/application/VisitStatisticsJobTest.java
+++ start/src/test/java/cn/itmtx/ddd/ezlink/application/VisitStatisticsJobTest.java
@@ -8,6 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 集成测试，需要确保环境都正常启动
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = Application.class)
 public class VisitStatisticsJobTest {
